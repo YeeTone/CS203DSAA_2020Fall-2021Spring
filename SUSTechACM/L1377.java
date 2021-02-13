350216c7866946b60f453d46e0d6ec23f5187858
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-02-13 11:32:53 +0800

    Add files via upload

---
+++ SUSTechACM/L1377.java
@@ -0,0 +1,176 @@
+package SUSTechACM;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class L1377 {
+    public static void main(String[] args) {
+        FastReader fastReader=new FastReader(System.in);
+        FastWriter fastWriter=new FastWriter(System.out);
+
+        char[]str=fastReader.next().toCharArray();
+        int[]next=getNext(str);
+
+        for (int j : next) {
+            fastWriter.println(j);
+        }
+
+        fastReader.close();
+        fastWriter.close();
+    }
+    private static int[] getNext(char[]patternStr){
+        int m=patternStr.length;
+        int[]next=new int[m];
+        next[0]=0;
+        int k=0,j=1;
+
+        while (j<m){
+            if(patternStr[j]==patternStr[k]){
+                k++;
+                next[j]=k;
+                j++;
+            }else if(k==0){
+                next[j]=0;
+                j++;
+            }else {
+                k=next[k-1];
+            }
+        }
+
+        return next;
+    }
+
+    private static class FastReader implements Closeable {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(InputStream in) {
+            br = new BufferedReader(new InputStreamReader(in), 16384);
+            eat("");
+        }
+
+        public void eat(String s) {
+            st = new StringTokenizer(s);
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public boolean hasNext() {
+            while(!st.hasMoreTokens()) {
+                String s = nextLine();
+                if(s==null) return false;
+                eat(s);
+            }
+            return true;
+        }
+
+        public String next() {
+            hasNext();
+            return st.nextToken();
+        }
+
+        public boolean nextBoolean(){
+            return Boolean.parseBoolean(next());
+        }
+
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public float nextFloat(){
+            return Float.parseFloat(next());
+        }
+
+        public double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        public BigInteger nextBigInteger(){
+            return new BigInteger(next());
+        }
+
+        public BigDecimal nextBigDecimal(){
+            return new BigDecimal(next());
+        }
+
+        public void close(){
+            try{
+                st=null;
+                br.close();
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+    }
+
+    private static class FastWriter implements Closeable {
+        private final BufferedWriter writer;
+        public FastWriter(OutputStream out){
+            this.writer=new BufferedWriter(new OutputStreamWriter(out));
+        }
+
+        public void print(Object object){
+            try{
+                writer.write(object.toString());
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+        public void printf(String format,Object... os) {
+            try{
+                writer.write(String.format(format,os));
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+        public void println(){
+            try{
+                writer.write(System.lineSeparator());
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        public void println(Object object) {
+            try{
+                writer.write(object.toString());
+                writer.write(System.lineSeparator());
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+
+        @Override
+        public void close(){
+            try{
+                writer.flush();
+                writer.close();
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+}
